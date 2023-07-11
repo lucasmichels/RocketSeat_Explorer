714994ba6de51a8945328b123be52464ad13bbb6
Lucas Sora Michels <39132280+lucasmichels@users.noreply.github.com>
2023-07-11 12:31:16 -0300

    Add files via upload

---
+++ Intensivo/JavaScript_Exercises/main.js
@@ -0,0 +1,104 @@
+/*
+// Exercício 01
+alert("Hello World!")
+*/
+
+/*
+// Exercício 02
+let numberOne = 10
+let numberTwo = 5
+
+sum = numberOne + numberTwo
+
+alert(sum)
+*/
+
+/*
+// Exercício 03
+let variable = 0
+
+let type = typeof(variable)
+
+if(type === 'number') {
+  alert("É um número")
+} else {
+  alert("Não é um número")
+}
+*/
+
+/*
+// Exercício 04
+let variable = 'vasco'
+
+let type = typeof(variable)
+
+if(type === 'string') {
+  alert("É uma string")
+} else {
+  alert("Não é uma string")
+}
+*/
+
+/*
+// Exercício 05
+let variable = true
+
+let type = typeof(variable)
+
+if(type === 'boolean') {
+  alert("É um booleano")
+} else {
+  alert("Não é um booleano")
+}
+*/
+
+/*
+// Exercício 06
+let numberOne = 10
+let numberTwo = 5
+
+sub = numberOne - numberTwo
+
+alert(sub)
+*/
+
+/*
+// Exercício 07
+let numberOne = 10
+let numberTwo = 5
+
+multi = numberOne * numberTwo
+
+alert(multi)
+*/
+
+/*
+// Exercício 08
+let numberOne = 10
+let numberTwo = 5
+
+div = numberOne / numberTwo
+
+alert(div)
+*/
+
+/*
+// Exercício 09
+let number = 11
+
+if((number % 2) == 0) {
+  alert("É um número par")
+} else {
+  alert("Não é um número par")
+}
+*/
+
+
+// Exercício 10
+let number = 11
+
+if((number % 2) != 0) {
+  alert("É um número ímpar")
+} else {
+  alert("Não é um número ímpar")
+}
